ec77f4bd0e406e8036d3449bbf68f441177e59d3
all102 <all102@txstate.edu>
2018-09-27 18:10:57 -0500

    initial commit

---
+++ src/threesolid/IProduct.java
@@ -0,0 +1,12 @@
+package threesolid;
+
+/** @author Allen Van
+ *
+ * Create a new interface for ProductManager specificially.
+ */
+
+public interface IProduct {
+
+    public void defineProduct();
+
+}
